8312d402206c45623c3988cb12a83dc3ecd64e49
Muhammad Imad <imadehsan1@gmail.com>
2025-04-25 03:44:59 +0500

    Update axiosinstances.js

--- clientui/src/redux/feature/services/axiosinstances.js
+++ clientui/src/redux/feature/services/axiosinstances.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosinstances = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://my-restaurant-backend.onrender.com',
     // headers: {
     //     'Authorization': `Bearer ${localStorage.getItem('token')}`,
     // },
