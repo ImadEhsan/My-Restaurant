d15a99a92131eb82bc4f39806943ed1abd0200bd
Muhammad Imad <imadehsan1@gmail.com>
2025-04-25 05:08:51 +0500

    Update StripeKey.js

--- backend/config/StripeKey.js
+++ backend/config/StripeKey.js
@@ -1,15 +1,8 @@
-// config/stripeKeys.js
+
 import dotenv from 'dotenv';
 dotenv.config();
 
-
-const   secretKey= process.env.STRIPE_SECRET_KEY
-const   publishableKey= process.env.STRIPE_PUBLISHABLE_KEY
+export const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
 
 
 
-const success_url= "http://localhost:3000/OrderSuccess"
-const cancel_url= "http://localhost:3000/cart"
-
-
-export { cancel_url,success_url,secretKey,publishableKey};
